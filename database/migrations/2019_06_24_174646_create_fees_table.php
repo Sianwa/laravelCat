fb64bcd1a2683b531b0ef7495256a1e8186b4f0d
Sianwa <32924825+Sianwa@users.noreply.github.com>
2019-06-25 16:52:19 +0300

    saved

--- database/migrations/2019_06_24_174646_create_fees_table.php
+++ database/migrations/2019_06_24_174646_create_fees_table.php
@@ -14,7 +14,7 @@ class CreateFeesTable extends Migration
     public function up()
     {
         Schema::create('fees', function (Blueprint $table) {
-            $table->bigIncrements('Fid');
+            $table->bigIncrements('fid');
             $table->unsignedBigInteger('id');
             $table->foreign('id')->references('id')->on('students');
             $table->integer('amount');
